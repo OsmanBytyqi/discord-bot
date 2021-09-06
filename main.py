a6d75dc4ba8a53f6c1cadfde8e2f58066b07b360
OsmanBytyqi <obytyqi2001@gmail.com>
2021-09-06 23:18:53 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,35 @@
+import discord
+
+
+
+client = discord.Client()
+
+
+@client.event
+async def on_ready():
+    print('Bot is now online and ready for work')
+
+
+    
+
+
+@client.event
+async def on_message(message):
+
+    if message.author == client.user:
+        return
+
+    if message.content == 'stres shume':
+        
+        await message.channel.send('`it\'s gonna be okay! :)` ')
+
+    if message.content=='depresion':
+         await message.channel.send('`it\'s gonna be okay! :)` ')
+
+    if message.content=='neser kam provim':
+         await message.channel.send('`Good luck !!! :)` ')
+
+
+
+
+client.run('')
