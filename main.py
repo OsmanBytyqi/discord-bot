97243d0509a88d1487e43fb42fe99f455d5d12e7
OsmanBytyqi <obytyqi2001@gmail.com>
2021-09-09 11:07:51 +0200

    :abc:

--- main.py
+++ main.py
@@ -10,23 +10,17 @@ async def on_ready():
     print('Bot is now online and ready for work')
 
 
+
+
 @client.event
 async def on_message(message):
 
     if message.author == client.user:
         return
 
-    if message.content == 'stres shume':
-        
-        await message.channel.send('`it\'s gonna be okay! :)` ')
+    if message.content == 'hello':
+        await message.channel.send('Welcome to thenewboston!')
 
-    if message.content=='depresion':
-         await message.channel.send('`it\'s gonna be okay! :)` ')
-
-    if message.content=='neser kam provim':
-         await message.channel.send('`Good luck !!! :)` ')
-    if message.content=='kur??'or message.content=='kur???' or message.content=='kur????':
-         await message.channel.send('**Very soon**  ')
          
 
     
