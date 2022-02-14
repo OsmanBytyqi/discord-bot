9d4ae2ece36e7ab342272eda107e4bddeef5a5d7
OsmanBytyqi <obytyqi2001@gmail.com>
2022-02-14 20:50:15 +0100

    uncomment the code

--- main.py
+++ main.py
@@ -1,68 +1,68 @@
-# import discord
-# import os
+ import discord
+ import os
 
-# from discord import message
-
-
-# client = discord.Client()
-
- 
-# @client.event
-# async def on_ready():
-#     print('Bot is now online and ready for work')
+ from discord import message
 
 
+ client = discord.Client()
 
 
-# @client.event
-# async def on_message(message):
+ @client.event
+ async def on_ready():
+     print('Bot is now online and ready for work')
 
 
 
 
-#     if message.author == client.user:
-#         return
+ @client.event
+ async def on_message(message):
 
-#     if message.content == 'hello':
-#         await message.channel.send('Welcome to our server')
 
+
+
+     if message.author == client.user:
+         return
+
+     if message.content == 'hello':
+         await message.channel.send('Welcome to our server')
+
+  
+     if message.content == 'stres'or message.content == 'stres shume'or message.content == 'shume stres':
+         await message.channel.send('**it\'s gonna be okay**')
+
+
+
+ @client.event
+ async def on_message(message):
+     if len(message.attachments) > 0: #Checks if there are attachments
+        pic_ext = ['.jpg','.png','.jpeg','.gif']
+        for file in message.attachments:
+             for ext in pic_ext:
+                 if file.filename.endswith(ext):
+                     await message.channel.send('HAHAHAHAHHAHAHAHAHAH ')
+
+
+
+
+     
+
+ @client.event
+ async def on_message_edit(before, after):
+     await before.channel.send(
+         f'{before.author} edit a message.\n'
+         f'Before: {before.content}\n'
+         f'After: {after.content}'
+     )
+
+
+ @client.event
+ async def on_reaction_add(reaction, user):
+     await reaction.message.channel.send(f'{user} reacted with {reaction.emoji}')
    
-#     if message.content == 'stres'or message.content == 'stres shume'or message.content == 'shume stres':
-#         await message.channel.send('**it\'s gonna be okay**')
 
 
 
-# @client.event
-# async def on_message(message):
-#     if len(message.attachments) > 0: #Checks if there are attachments
-#        pic_ext = ['.jpg','.png','.jpeg','.gif']
-#        for file in message.attachments:
-#             for ext in pic_ext:
-#                 if file.filename.endswith(ext):
-#                     await message.channel.send('HAHAHAHAHHAHAHAHAHAH ')
-
-
-
-
-      
-
-# @client.event
-# async def on_message_edit(before, after):
-#     await before.channel.send(
-#         f'{before.author} edit a message.\n'
-#         f'Before: {before.content}\n'
-#         f'After: {after.content}'
-#     )
-
-
-# @client.event
-# async def on_reaction_add(reaction, user):
-#     await reaction.message.channel.send(f'{user} reacted with {reaction.emoji}')
-    
-
-
-
-# client.run(os.environ['MY_TOKEN'])
+ client.run(os.environ['MY_TOKEN'])
 
 
 
